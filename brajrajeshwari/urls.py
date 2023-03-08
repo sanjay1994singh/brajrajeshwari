c6d56d1f3b1ab1160f5280cf915b35cd17eb9367
sanjay <sanjaypython22@gmail.com>
2023-03-08 14:41:58 +0530

    third commit

--- brajrajeshwari/urls.py
+++ brajrajeshwari/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home_page.urls'),name='home_page')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
