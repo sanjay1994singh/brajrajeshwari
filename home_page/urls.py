b0c4c6e689fb3add9e662b4d60928cb83a4a814f
sanjay <sanjaypython22@gmail.com>
2023-03-09 13:05:07 +0530

    new changes

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from.import views
 urlpatterns = [
-    path('',views.home_page,name='home_page')
+    path('',views.home_page,name='home_page'),
+    path('product-details/<int:id>/',views.product_details,name='product_details'),
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -14,3 +14,15 @@ def home_page(request):
         'c_data':c_data,     
     }
     return render(request,'index.html',context)
+
+def product_details(request,id):
+    b_data = BrandMaster.objects.all()
+    p_data = ProductMaster.objects.get(id=id)
+    c_data = CategoryMaster.objects.all()
+
+    context = {
+        'b_data':b_data,
+        'p_data':p_data,
+        'c_data':c_data,     
+    }
+    return render(request,'product_details.html',context)
